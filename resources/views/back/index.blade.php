1bf4527feee535424fec8e43bf4c7e507a7342df
Mustafa <bayarmustafa151@gmail.com>
2020-12-28 02:08:58 +0300

    added index admin blade template

---
+++ resources/views/back/index.blade.php
@@ -0,0 +1,7 @@
+@extends('back.layouts.dashboard')
+@section('title','Admin Dashboard')
+
+@section('content')
+    @include('back.layouts.content')
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Back-End Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+*/
+
+Route::get('/admin','App\Http\Controllers\Admin\HomeController@index');
+
+
+/*
+|--------------------------------------------------------------------------
+| Front-End Routes
+|--------------------------------------------------------------------------
 */
 
 Route::get('/','App\Http\Controllers\HomeController@index')->name('homepage');
