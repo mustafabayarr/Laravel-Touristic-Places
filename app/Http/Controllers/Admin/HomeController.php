ae3061a3b2dadc2368612adcd1bfcd9656f3cdf8
Mustafa <bayarmustafa151@gmail.com>
2020-12-28 23:31:54 +0300

    login transactions have been done

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,11 +4,49 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('back.index');
+    }
+
+    public function login()
+    {
+        return view('back.admin-login');
+    }
+
+    public function login_check(Request $request)
+    {
+        if ($request->isMethod('POST')) {
+            $credentials = $request->only('email', 'password');
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+
+                return redirect()->intended('admin_index');
+            }
+
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+                'password' => 'The provided credentials do not match our records.',
+            ]);
+        } else {
+            return view('admin_login');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Back-End Routes
 |--------------------------------------------------------------------------
 */
 
-Route::get('/admin','App\Http\Controllers\Admin\HomeController@index');
+Route::get('/admin',[App\Http\Controllers\Admin\HomeController::class,'index'])->name('admin_index')->middleware('auth');
+Route::get('/admin/login',[App\Http\Controllers\Admin\HomeController::class,'login'])->name('admin_login');
+Route::post('/admin/login_check',[App\Http\Controllers\Admin\HomeController::class,'login_check'])->name('admin_login_check');
+Route::get('/admin/logout',[App\Http\Controllers\Admin\HomeController::class,'logout'])->name('admin_logout');
 
 
 /*
@@ -17,16 +21,16 @@ Route::get('/admin','App\Http\Controllers\Admin\HomeController@index');
 |--------------------------------------------------------------------------
 */
 
-Route::get('/','App\Http\Controllers\HomeController@index')->name('homepage');
-Route::get('/about','App\Http\Controllers\HomeController@about')->name('about');
-Route::get('/categories','App\Http\Controllers\HomeController@categories')->name('categories');
-Route::get('/listing','App\Http\Controllers\HomeController@listing')->name('listing');
-Route::get('/contact','App\Http\Controllers\HomeController@contact')->name('contact');
-Route::get('/listing_details','App\Http\Controllers\HomeController@listing_details')->name('listing_details');
+Route::get('/',[App\Http\Controllers\HomeController::class,'index'])->name('homepage');
+Route::get('/about',[App\Http\Controllers\HomeController::class,'about'])->name('about');
+Route::get('/categories',[App\Http\Controllers\HomeController::class,'categories'])->name('categories');
+Route::get('/listing',[App\Http\Controllers\HomeController::class,'listing'])->name('listing');
+Route::get('/contact',[App\Http\Controllers\HomeController::class,'contact'])->name('contact');
+Route::get('/listing_details',[App\Http\Controllers\HomeController::class,'listing_details'])->name('listing_details');
 /*
 Route::get('/', function () {
-    return view('homepage');
-})->name('homepage');*/
+    return view('welcome');
+})->name('welcome');*/
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
